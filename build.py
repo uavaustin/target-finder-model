a7a83acda5d0ef80bcc4d1a126b2352f3c776a74
Shrivu Shankar <shrivu1122@gmail.com>
2019-02-14 20:25:33 -0600

    CI Fix 3

--- build.py
+++ build.py
@@ -7,7 +7,7 @@ import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'generate'))
 
-
+from pull_assets import pull_all
 from create_full_images import generate_all_shapes
 from create_detection_data import convert_data as det_convert_data
 from create_clf_data import convert_data
@@ -17,6 +17,8 @@ if __name__ == '__main__':
     # Make sure Windows doesn't blow up with the multiprocessing in
     # the shape generation script.
     multiprocessing.freeze_support()
+
+    pull_all()
 
     generate_all_shapes('test', 10)
     det_convert_data('test', 10)
