3740a12d1c3e29b9a2e11e8e3c353c78efedb759
Shrivu Shankar <shrivu1122@gmail.com>
2019-02-14 20:54:47 -0600

    CI Fix 5

--- generate/build.py
+++ generate/build.py
@@ -4,9 +4,6 @@ import multiprocessing
 import os.path
 import sys
 
-
-sys.path.append(os.path.join(os.path.dirname(__file__), 'generate'))
-
 from pull_assets import pull_all
 from create_full_images import generate_all_shapes
 from create_detection_data import convert_data as det_convert_data
@@ -20,6 +17,6 @@ if __name__ == '__main__':
 
     pull_all()
 
-    generate_all_shapes('test', 10)
-    det_convert_data('test', 10)
-    convert_data('test', 10)
+    generate_all_shapes('testing', 10)
+    det_convert_data('testing', 10)
+    convert_data('testing', 10)
