41ee015e574fc2a30346ce6942ae52d9fed4cedc
Shrivu Shankar <shrivu1122@gmail.com>
2019-03-07 19:52:14 -0600

    Added Unit Tests

--- test/test_model.py
+++ test/test_model.py
@@ -2,7 +2,7 @@
 
 import os
 
-import target_finder_model
+import target_finder_model as tfm
 
 
 WEIGHTS_PATH = os.path.join('model', 'weights')
@@ -10,6 +10,16 @@ YOLO_WEIGHTS = os.path.join(WEIGHTS_PATH, 'yolo3detector-train_final.weights')
 CLF_WEIGHTS = os.path.join(WEIGHTS_PATH, 'preclf-train_final.weights')
 
 
-def test_model():
+def test_constants():
+    """Test constants packaged with tfm"""
+    assert tfm.CROP_SIZE[0] == tfm.CROP_SIZE[1]
+    assert tfm.CROP_OVERLAP < tfm.CROP_SIZE[0]
+
+    assert tfm.DETECTOR_SIZE[0] == tfm.DETECTOR_SIZE[1]
+    assert tfm.PRECLF_SIZE[0] == tfm.PRECLF_SIZE[1]
+
+
+def test_weights_exist():
+    """Ensure weights were produced"""
     assert os.path.exists(YOLO_WEIGHTS)
     assert os.path.exists(CLF_WEIGHTS)
