bc22456cd4562c7ce86792f4687304e6dd2a8fc5
Shrivu Shankar <shrivu1122@gmail.com>
2019-03-07 19:58:15 -0600

    Rerearranged Tests

--- test/test_model.py
+++ test/test_model.py
@@ -5,11 +5,6 @@ import os
 import target_finder_model as tfm
 
 
-WEIGHTS_PATH = os.path.join('model', 'weights')
-YOLO_WEIGHTS = os.path.join(WEIGHTS_PATH, 'yolo3detector-train_final.weights')
-CLF_WEIGHTS = os.path.join(WEIGHTS_PATH, 'preclf-train_final.weights')
-
-
 def test_constants():
     """Test constants packaged with tfm"""
     assert tfm.CROP_SIZE[0] == tfm.CROP_SIZE[1]
@@ -18,8 +13,5 @@ def test_constants():
     assert tfm.DETECTOR_SIZE[0] == tfm.DETECTOR_SIZE[1]
     assert tfm.PRECLF_SIZE[0] == tfm.PRECLF_SIZE[1]
 
-
-def test_weights_exist():
-    """Ensure weights were produced"""
-    assert os.path.exists(YOLO_WEIGHTS)
-    assert os.path.exists(CLF_WEIGHTS)
+    assert len(tfm.YOLO_CLASSES) > 0
+    assert len(tfm.CLF_CLASSES) > 0
