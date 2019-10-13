cc968de9c71a41d0ea768dc418418e46fcda7953
ZeptoByte <42948620+ZeptoByte@users.noreply.github.com>
2019-10-13 15:57:17 -0500

    Revert "Merge branch 'master' of https://github.com/uavaustin/target-finder-model"
    This reverts commit daefb31d55f671fdd9d72a02f3425b6a0a087c99, reversing
    changes made to eb02703c707bebbbec65d0c5e92b76f893725bd3.

--- generate/build.py
+++ generate/build.py
@@ -11,7 +11,12 @@ from create_clf_data import convert_data as clf_convert_data
 
 
 if __name__ == '__main__':
+    # Make sure Windows doesn't blow up with the multiprocessing in
+    # the shape generation script.
+    multiprocessing.freeze_support()
+
     pull_all()
+
     generate_all_shapes('testing', 5)
     det_convert_data('testing', 5)
     clf_convert_data('testing', 5)
