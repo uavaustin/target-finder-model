62663a7de415f60d204ed1485faa33d2a54ffc74
alexwitt2399 <awitt2399@utexas.edu>
2019-11-14 01:33:09 -0600

    first rendition of a crop data cenerator for orientation

--- scripts_generate/build.py
+++ scripts_generate/build.py
@@ -7,6 +7,7 @@ import sys
 from pull_assets import pull_all
 from create_detection_data import generate_all_images as create_det_data
 from create_clf_data import create_clf_images as create_clf_data
+from create_orientation_data import generate_all_images as create_ori_data
 
 
 if __name__ == '__main__':
@@ -18,3 +19,6 @@ if __name__ == '__main__':
 
     create_clf_data('detector_train', 5)
     create_clf_data('detector_val', 5)
+
+    create_orientation_data('orientation_data', 5)
+    create_orientation_data('orientation_data', 5)
