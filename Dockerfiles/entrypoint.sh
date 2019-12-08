fa61392cc8d3989c8e0a345c66e1e9bdefdb1153
alexwitt2399 <awitt2399@utexas.edu>
2019-12-08 01:56:55 -0600

    put tf-record gen back in ci

--- Dockerfiles/entrypoint.sh
+++ Dockerfiles/entrypoint.sh
@@ -4,6 +4,10 @@ pycodestyle --exclude=vendor target_finder_model generate scripts_tf test
 
 python scripts_generate/build.py
 
+python scripts_tf/create_tf_records.py
+mv model_data /host/model_data
+
+
 python test/test_models.py
 
 pytest --cov=target_finder_model test
