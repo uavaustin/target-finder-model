6a908fd0c95c5853f49488ab25a08ab26048d067
alexwitt2399 <awitt2399@utexas.edu>
2019-12-08 02:19:02 -0600

    mv to wrong place

--- Dockerfiles/entrypoint.sh
+++ Dockerfiles/entrypoint.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-pycodestyle --exclude=vendor target_finder_model generate scripts_tf test
+pycodestyle --exclude=vendor --ignore=E402 target_finder_model generate scripts_tf test
 
 python scripts_generate/build.py
 
 python scripts_tf/create_tf_records.py
-mv model_data /host/model_data
-
+mv model_data /host
 
 python test/test_models.py
 
 pytest --cov=target_finder_model test
 
-cp .coverage* /host
+mv .coverage* /host
