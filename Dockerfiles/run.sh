45d6e19144a48f340e3f1e8b60fb885f5efd6f7d
alexwitt2399 <awitt2399@utexas.edu>
2020-03-15 22:56:20 -0700

    Overhaul

--- Dockerfiles/run.sh
+++ Dockerfiles/run.sh
@@ -3,6 +3,6 @@ xhost +"local:docker@"
 sudo docker run --runtime=nvidia -ti --net=host --ipc=host -e DISPLAY=$DISPLAY \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
 -v $PWD:/host \
-uavaustin/target-finder-model-env:tf1 /bin/bash
+uavaustin/target-finder-model-env:tf2 /bin/bash
 
 xhost -
